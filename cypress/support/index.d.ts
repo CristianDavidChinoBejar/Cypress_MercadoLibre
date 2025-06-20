29207ef4ec111e81e65415150e0125ae3150b1cc
CristianDavidChinoBejar <cristianrg095@gmail.com>
2025-06-20 05:22:52 -0300

    mod delete login command

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -4,7 +4,6 @@ declare global {
     namespace Cypress {
         interface Chainable {
             searchProduct(product: string): Chainable<Element>;
-            login(email: string, password: string): Chainable<Element>;
         }
     }
 }
